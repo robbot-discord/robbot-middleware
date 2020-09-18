0a2fd2ad673c5749263e7446e2bd1defe5e42861
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-09-18 03:22:59 -0400

    Update per-server-event-handlers for new types

--- packages/per-server-event-handlers/src/index.ts
+++ packages/per-server-event-handlers/src/index.ts
@@ -1,27 +1,52 @@
 import { PerServerEventHandlers } from "./types"
-import { EventHandlers } from "@robbot/robbot-core/dist/handlers"
+import { EventCreator, EventHandlers } from "@robbot/robbot-core"
 import { produce } from "immer"
-import { EventHandlerMiddleware } from "@robbot/robbot-core/dist/middleware/types"
+import { EventHandlerMiddleware } from "@robbot/robbot-core"
+import { RobBotClient } from "@robbot/robbot-core"
 
 export const createServerFilterMiddleware = (
   configuration: PerServerEventHandlers
 ): EventHandlerMiddleware => {
-  return (eventHandlers: EventHandlers) => {
-    return produce(eventHandlers, (draft) => {
-      draft.message = (message) => {
-        if (message.guild) {
-          const guildSnowflake = message.guild.id
-          const guildMessageHandler = configuration[guildSnowflake]?.message
+  // TODO is there a way to write this in ES6 () => {} syntax?
+  function newMiddleware(eventHandlers: EventHandlers): EventHandlers
+  function newMiddleware(eventCreator: EventCreator): EventCreator
 
-          if (guildMessageHandler) {
-            guildMessageHandler(message)
-          } else {
-            eventHandlers.message(message)
+  function newMiddleware(eventHandlersOrCreator: unknown): unknown {
+    const isFunction = typeof eventHandlersOrCreator === "function"
+    const isObject = typeof eventHandlersOrCreator === "object"
+
+    if (isFunction) {
+      const eventCreator = (eventHandlersOrCreator as unknown) as EventCreator
+      const returnValue = (client: RobBotClient) => {
+        return eventCreator(client)
+      }
+
+      return returnValue
+    } else if (isObject) {
+      const eventHandlers = (eventHandlersOrCreator as unknown) as EventHandlers
+      const newEventHandlers = produce(eventHandlers, (draft) => {
+        draft.message = (message) => {
+          if (message.guild) {
+            const guildSnowflake = message.guild.id
+            const guildMessageHandler = configuration[guildSnowflake]?.message
+
+            if (guildMessageHandler) {
+              guildMessageHandler(message)
+            } else {
+              eventHandlers.message(message)
+            }
           }
         }
-      }
-    })
+      })
+
+      return newEventHandlers
+    }
+
+    // in case of incorrect argument type, no-op
+    return eventHandlersOrCreator
   }
+
+  return newMiddleware
 }
 
 export * from "./types"
